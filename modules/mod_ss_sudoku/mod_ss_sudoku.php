8d36fce1566850bc0008df30e823f0bc23c5ff41
Chenglong GUO <guochenglong0505@gmail.com>
2018-03-02 17:54:55 +0100

    update joomla 3.8

--- modules/mod_ss_sudoku/mod_ss_sudoku.php
+++ modules/mod_ss_sudoku/mod_ss_sudoku.php
@@ -2,8 +2,8 @@
 // No direct access
 defined('_JEXEC') or die;
 
-// Include the syndicate functions only once
-require_once dirname(__FILE__) . '/helper.php';
+// Include the feed functions only once
+JLoader::register('ModSSSudokuHelper', __DIR__ . '/helper.php');
 
 $hello = ModSSSudokuHelper::getHello();
 
